54561cdbf3365733acf68c24b5113d6a2b732fc7
VaskaDaGama26 <131616200+VaskaDaGama26@users.noreply.github.com>
2024-11-24 20:23:29 +0300

    Update sqlite_module.py

--- sqlite_module.py
+++ sqlite_module.py
@@ -1,9 +1,20 @@
 import sqlite3
+import os
 
 class SQliteDB:
+    def __init__(self):
+        # Получаем путь к базе данных в %APPDATA%
+        self.db_path = os.path.join(os.getenv('APPDATA'), 'ExpenseApp', 'expenses.db')
+
+        # Убедитесь, что папка существует
+        os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
+
+        # Если база данных отсутствует, создаем её
+        if not os.path.exists(self.db_path):
+            self.create_db()
 
     def create_db(self):
-        conn = sqlite3.connect('expenses.db')
+        conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute("""CREATE TABLE IF NOT EXISTS expenses (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,7 +26,7 @@ class SQliteDB:
         conn.close()
 
     def insert_value(self, amount,category,desc):
-        conn = sqlite3.connect('expenses.db')
+        conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute("""INSERT INTO expenses (amount, category, desc) VALUES (:amount, :category, :desc)""",
                         {'amount': amount,'category': category,'desc': desc}
@@ -24,7 +35,7 @@ class SQliteDB:
         conn.close()
 
     def delete_value(self, amount, category, desc):
-        conn = sqlite3.connect('expenses.db')
+        conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute("DELETE FROM expenses WHERE amount = :amount AND category = :category AND desc = :desc",
                        {'amount': amount, 'category': category, 'desc': desc})
@@ -32,7 +43,7 @@ class SQliteDB:
         conn.close()
 
     def fetch_category(self, category):
-        conn = sqlite3.connect('expenses.db')
+        conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM expenses WHERE category=category")
         rows = cursor.fetchall()
@@ -42,7 +53,7 @@ class SQliteDB:
         conn.close()
 
     def fetch_all_records(self):
-        conn = sqlite3.connect('expenses.db')
+        conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM expenses")
         rows = cursor.fetchall()
